0b7b04b90a9401a9ee78e7536b1dcb2760bd9b57
Pei Chang <pchang@hawaiirg.com>
2016-03-18 08:29:02 -0700

    add cluster and ewd in memory queue

--- fmql/ewd/nonClusterApp.js
+++ fmql/ewd/nonClusterApp.js
@@ -2,29 +2,83 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var qoper8 = require('ewd-qoper8');
 var qx = require('ewd-qoper8-express');
+var cluster = require('cluster');
 
-var app = express();
-app.use(bodyParser.json());
 
-var q = new qoper8.masterProcess();
-qx.addTo(q);
+function workAround(worker) {
+    var listeners = null;
 
-app.post('/qoper8', function(req, res) {
-    qx.handleMessage(req, res);
-});
+    listeners = worker.process.listeners('exit')[0];
+    var exit = listeners[Object.keys(listeners)[0]];
 
-app.get('/qoper8/test', function(req, res) {
-    qx.handleMessage(req, res);
-});
+    listeners = worker.process.listeners('disconnect')[0];
+    var disconnect = listeners[Object.keys(listeners)[0]];
 
-app.get('/fmqlEP', function(request, response) {
-    
-    qx.handleMessage(request, response);
-});
+    worker.process.removeListener('exit', exit);
+    worker.process.once('exit', function(exitCode, signalCode) {
+        if (worker.state != 'disconnected')
+            disconnect();
+        exit(exitCode, signalCode);
+    });
+}
 
-q.on('started', function() {
-    this.worker.module = '/home/vdp/fmql/ewd/workerModule';
-    var server = app.listen(9000);
-});
+/* 
+ * Typical 'cluster' setup
+ */
+if (cluster.isMaster) {
 
-q.start();
+    var numCPUs = require('os').cpus().length;
+    console.log('cpus: ' + numCPUs);
+    // Create a worker for each CPU
+    for (var i = 0; i < numCPUs; i += 1) {
+        var newWorker = cluster.fork();
+        workAround(newWorker);
+
+    }
+
+    console.log("Master process %d (kill -9 this) with %d CPUs to play with", process.pid, numCPUs);
+
+    // Listen for dying workers
+    cluster.on('exit', function(worker, code, signal) {
+
+        // If forced, don't restart - uncaughtException forces with kill()
+        if (worker.suicide === true)
+            return;
+
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker %d died :( - starting a new one', worker.id);
+        console.log('==== died with code ' + code);
+        console.log('==== died with signal ' + signal);
+        var newWorker2 = cluster.fork();
+        workAround(newWorker2);
+
+    });
+
+    // workAround(cluster.worker);
+
+} else {
+    process.on('uncaughtException', function(err) {
+        // db.close();
+        console.trace('Uncaught Exception - exiting worker');
+        console.error(err.stack);
+        // exit(1) - Uncaught Fatal Exception
+        cluster.worker.kill();
+    });
+
+    var app = express();
+    app.use(bodyParser.json());
+
+    var q = new qoper8.masterProcess();
+    qx.addTo(q);
+
+    app.get('/fmqlEP', function(req, res) {
+        qx.handleMessage(req, res);
+    });
+
+    q.on('started', function() {
+        this.worker.module = '/home/vdp/fmql/ewd/workerModule';
+        var server = app.listen(9000);
+    });
+
+    q.start();
+}
--- fmql/ewd/workerModule.js
+++ fmql/ewd/workerModule.js
@@ -4,33 +4,47 @@ var compress = require("compression"),
     fmql = require('../fmql');
 var db = new nodem.Gtm();
 // { ok: 1, result: '1' }
-var ok = db.open();    
+// var ok = db.open();    
 
 module.exports = function() {
 
-  this.on('message', function(request, send, finished) {
-    // Enforce ?fmql="SOME QUERY" (rem: query arguments don't get routes of their own in Express)
-    if (!(("fmql" in request.query) && (request.query.fmql !== ""))) {
-        // response.status(404).json({
-        //     "error": "No FMQL Query Specified"
-        // });
-        console.log("404'ing: %s", request.url);
-        finished('error');
-        return;
+    this.on('message', function(request, send, finished) {
+        // Enforce ?fmql="SOME QUERY" (rem: query arguments don't get routes of their own in Express)
+        console.log('=====', request);
+        if (request.path.indexOf('/fmqlEP') === 0) {
+            fmqlEP(request, send, finished);
+        }
+    });
+
+    function fmqlEP(request, send, finished) {
+        if (!(("fmql" in request.query) && (request.query.fmql !== ""))) {
+            console.log("404'ing: %s", request.url);
+            var results = {
+                error: "No FMQL Query Specified",
+                status: {
+                    code: 403
+                }
+            };
+            finished(results);
+            return;
+        }
+        // {"query": "DESCRIBE 2-9"}
+        var query = request.query.fmql;
+        var jsont = fmql.query(db, query, false);
+        var results = {
+            query: query,
+            result: jsont,
+            worker: 'processed from worker ' + process.pid,
+            time: new Date().toString()
+        };
+        finished(results);
     }
 
-    // {"query": "DESCRIBE 2-9"}
-    var query = request.query.fmql;
+    this.on('start', function() {
+        db.open();
+    });
+    this.on('stop', function() {
+        db.close();
+    });
 
-    var jsont = fmql.query(db, query, false);
-
-    var results = {
-      query: query,
-      result: jsont,
-      worker: 'processed from worker ' + process.pid,
-      time: new Date().toString()
-    };
-    finished(results);
-  });
-  
 };
