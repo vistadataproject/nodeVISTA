8ba2041460f3a7a7465fbd9585ed4a5c486d45cf
conorcaregraf <conor-dowling@caregraf.com>
2016-03-19 18:08:41 -0700

    report on code

---
+++ VPRMUMPS/README.md
@@ -0,0 +1,1 @@
+From OSEHRA/vista-m repo - https://github.com/OSEHRA/VistA-M/tree/master/Packages/Virtual%20Patient%20Record/Routines
---
+++ eHMP/eHMPCodeParse.py
@@ -0,0 +1,81 @@
+import json
+import os
+import sys
+import re
+
+"""
+Parse eHMP MUMPS code for key information
+
+Particulars:
+- copy from VPR from original HMP
+- all RPCs (compare to static list)
+- ALL triggers
+- dates on files
+
+TODO: use KIDS parsers to do rawer parse
+"""
+
+EHMP_MUMPS_DIR = "HMPMUMPS"
+VPR_MUMPS_DIR = "../VPRMUMPS"
+
+"""
+HMP Files 148
+VPR copies 52
+Non HMP NS files 3
+HMP Non VPR Copies 96
+	Of Agilex 8
+	Of SLC 73
+	Of Other 15
+"""
+def describeFiles():
+
+    vprFilesLessNS = set(re.sub(r'^VPR', '', f) for f in os.listdir(VPR_MUMPS_DIR) if re.match(r'VPR', f))
+    
+    hmpFiles = set()
+    hmpVPRCopies = set()
+    hmpNonNSFiles = set()
+    for f in os.listdir(EHMP_MUMPS_DIR):
+        if not re.search(r'\.m$', f):
+            continue
+        hmpFiles.add(f)
+        if not re.match(r'HMP', f):
+            hmpNonNSFiles.add(f)
+            continue
+        fLessNS = re.sub(r'^HMP', '', f)
+        if fLessNS in vprFilesLessNS:
+            hmpVPRCopies.add(f)
+    
+    print "HMP Files", len(hmpFiles)
+    print "VPR copies", len(hmpVPRCopies)
+    print "Non HMP NS files", len(hmpNonNSFiles)
+    
+    hmpNonVPRCopies = hmpFiles - hmpVPRCopies
+    fmeta = {}
+    for f in hmpNonVPRCopies:
+        fmeta[f] = {}
+        for l in open(EHMP_MUMPS_DIR + "/" + f):
+            dtSearch = re.search(r'([A-Za-z\d \@\:\/\,]+)$', l)
+            if dtSearch and re.search(r'\d', dtSearch.group(1)):
+                fmeta[f]["date"] = dtSearch.group(1)
+            if re.search("SLC", l):
+                fmeta[f]["creator"] = "SLC"
+            elif re.search(r'(AGILEX|Agilex)', l):
+                fmeta[f]["creator"] = "AGILEX"
+            else:
+                fmeta[f]["creator"] = "OTHER"
+            break
+    # TODO: fix dates better and report 13/14/15/16 ...
+    print "HMP Non VPR Copies", len(hmpNonVPRCopies)
+    print "\tOf Agilex", sum(1 for f in fmeta if fmeta[f]["creator"] == "AGILEX")
+    print "\tOf SLC", sum(1 for f in fmeta if fmeta[f]["creator"] == "SLC")
+    print "\tOf Other", sum(1 for f in fmeta if fmeta[f]["creator"] == "OTHER")
+    print
+    for f in fmeta:
+        print f, "" if "date" not in fmeta[f] else fmeta[f]["date"]
+    
+def main():
+
+    describeFiles()
+    
+if __name__ == "__main__":
+    main()
