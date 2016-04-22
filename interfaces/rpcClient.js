7102892ea83230a53731f85ceaca710e231f9fba
Pei Chang <pchang@hawaiirg.com>
2016-04-21 22:36:30 -0700

    add rpc js client

---
+++ interfaces/rpcClient.js
@@ -0,0 +1,42 @@
+var request = require('request');
+
+request.post({
+        url: 'http://localhost:9001/vista/login',
+        json: {
+            "accessCode": "fakenurse1",
+            "verifyCode": "NEWVERIFY1!"
+        }
+    },
+    function(error, response, body) {
+        if (error) {
+            return console.error(' failed:', error);
+        }
+        if (!error && response.statusCode == 200) {
+            console.log(response)
+            var token = response.body.token;
+            runRPC('DDR GETS ENTRY DATA', token);
+        }
+    }
+);
+
+
+function runRPC(rpc, authToken) {
+    request.post({
+        url: 'http://localhost:9001/vista/runRPC/' + rpc,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authToken
+        }
+    },
+    function(error, response, body) {
+        if (error) {
+            return console.error(' failed:', error);
+        }
+        if (!error && response.statusCode == 200) {
+            console.log(response);
+            console.log(body);
+        }
+        console.log(body);
+    }
+);
+}
