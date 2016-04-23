f99b1ab7024169fcdf4c077703bca891ea4906f0
Pei Chang <pchang@hawaiirg.com>
2016-04-22 21:41:31 -0700

    update rpcClient.js

--- interfaces/rpcClient.js
+++ interfaces/rpcClient.js
@@ -1,41 +1,46 @@
 var request = require('request');
 
-request.post({
-        url: 'http://localhost:9001/vista/login',
-        json: {
-            "accessCode": "fakenurse1",
-            "verifyCode": "NEWVERIFY1!"
-        }
-    },
-    function(error, response, body) {
-        if (error) {
-            return console.error(' failed:', error);
-        }
-        if (!error && response.statusCode == 200) {
-            console.log(response)
-            var token = response.body.token;
-            runRPC('ORWU USERINFO', token);
-        }
-    }
-);
+rpc = process.argv[2] || 'XUS GET USER INFO';
 
+function login(callback) {
+    request.post({
+            url: 'http://localhost:9001/vista/login',
+            json: {
+                "accessCode": "fakenurse1",
+                "verifyCode": "NEWVERIFY1!"
+            }
+        },
+        function(error, response, body) {
+            if (error) {
+                return console.error(' failed:', error);
+            }
+            if (!error && response.statusCode == 200) {
+                // console.log(response)
+                var token = response.body.token;
+                callback(rpc, token);
+            }
+        }
+    );
+}
 
 function runRPC(rpc, authToken) {
     request.post({
-        url: 'http://localhost:9001/vista/runRPC/' + rpc,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authToken
+            url: 'http://localhost:9001/vista/runRPC/' + rpc,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': authToken
+            }
+        },
+        function(error, response, body) {
+            if (error) {
+                return console.error(' failed:', error);
+            }
+            if (!error && response.statusCode == 200) {
+                //console.log(response);
+                console.log(body);
+            }
         }
-    },
-    function(error, response, body) {
-        if (error) {
-            return console.error(' failed:', error);
-        }
-        if (!error && response.statusCode == 200) {
-            //console.log(response);
-            console.log(body);
-        }
-    }
-);
+    );
 }
+
+login(runRPC);
