82403b660012b0a233ee8c747a4f70574c75a3a8
Pei Chang <pchang@hawaiirg.com>
2016-04-26 14:41:10 -0700

    rpc client (debug) version

--- interfaces/rpcClient.js
+++ interfaces/rpcClient.js
@@ -1,12 +1,25 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 var request = require('request');
 
-rpc = process.argv[2] || 'XUS GET USER INFO';
+//rpc = process.argv[2] || 'XUS GET USER INFO';
+rpc = process.argv[2] || 'ORWPT LIST ALL';
+rpcArgs = process.argv[3];
 
 function login(callback) {
+    request.defaults({
+      strictSSL: false, // allow us to use our self-signed cert for testing
+      rejectUnauthorized: false
+    });
+
     request.post({
-            url: 'http://localhost:9001/vista/login',
+            strictSSL: false, // allow us to use our self-signed cert for testing
+            rejectUnauthorized: false,
+
+            url: 'https://localhost:9001/vista/login',
+            rejectUnhauthorized : false,
             json: {
-                "accessCode": "fakenurse1",
+                "accessCode": "fakedoc1", //"fakenurse1",
                 "verifyCode": "NEWVERIFY1!"
             }
         },
@@ -17,19 +30,29 @@ function login(callback) {
             if (!error && response.statusCode == 200) {
                 // console.log(response)
                 var token = response.body.token;
-                callback(rpc, token);
+                if (callback) callback(rpc, token);
             }
         }
     );
 }
 
 function runRPC(rpc, authToken) {
+    request.defaults({
+      strictSSL: false, // allow us to use our self-signed cert for testing
+      rejectUnauthorized: false
+    });
     request.post({
-            url: 'http://localhost:9001/vista/runRPC/' + rpc,
+            url: 'https://localhost:9001/vista/runRPC/' + rpc,
+            //      strictSSL: false, // allow us to use our self-signed cert for testing
+            //rejectUnhauthorized : false,
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': authToken
-            }
+              //  'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': authToken,
+            },
+            json: { "input" : "1", 
+                   "input" : "1" }
+            //json: rpcArgs
+            //formData: {rpcArgs: rpcArgs}
         },
         function(error, response, body) {
             if (error) {
@@ -37,7 +60,7 @@ function runRPC(rpc, authToken) {
             }
             if (!error && response.statusCode == 200) {
                 //console.log(response);
-                console.log(body);
+                console.log("/n *********client body log: "+ body);
             }
         }
     );
