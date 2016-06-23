f77320965861960047387f6aeb4dbc9a8595eb6a
Sean Ishii <sishii@hawaiirg.com>
2016-06-23 09:08:05 -1000

    Issue_54 BROKEN Added some unit tests defined in jasmine (still haven't got it to work yet)

---
+++ interfaces/rpcServer/spec/rpcUtils-spec.js
@@ -0,0 +1,194 @@
+#!javascript
+
+/**
+ * rpcUtils.js unit tests
+ */
+
+var rpcUtils = require('../rpcUtils.js');
+var VistaJSLibrary = require('../../VistaJS/VistaJSLibrary');
+
+console.log("uugh");
+
+describe ("unSPack", function () {
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_1_STRING_ALPHA +"' to payload '" + LENGTH_1_STRING_ALPHA + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_1_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_1_STRING_ALPHA);
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_8_STRING_ALPHA +"' to payload '" + LENGTH_8_STRING_ALPHA + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_8_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_8_STRING_ALPHA);
+
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_10_STRING_ALPHA +"' to payload '" + LENGTH_10_STRING_ALPHA + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_10_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_10_STRING_ALPHA);
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_1_STRING_NUM_0 +"' to payload '" + LENGTH_1_STRING_NUM_0 + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_1_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_1_STRING_NUM_0);
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_1_STRING_NUM_1 +"' to payload '" + LENGTH_1_STRING_NUM_1 + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_1_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_1_STRING_NUM_1);
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_8_STRING_NUM_0 +"' to payload '" + LENGTH_8_STRING_NUM_0 + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_8_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_8_STRING_NUM_0);
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_8_STRING_NUM_1 +"' to payload '" + LENGTH_8_STRING_NUM_1 + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_8_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_8_STRING_NUM_1);
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_10_STRING_NUM_0 +"' to payload '" + LENGTH_10_STRING_NUM_0 + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_10_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_10_STRING_NUM_0);
+
+    });
+
+    it ("should correctly un-SPack '"+ SPACK_LENGTH_10_STRING_NUM_1 +"' to payload '" + LENGTH_10_STRING_NUM_1 + "'", function () {
+        var payload = rpcUtils.unSPack(SPACK_LENGTH_10_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_10_STRING_NUM_1);
+    });
+
+});
+
+describe ("unLPack", function () {
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_1_STRING_ALPHA + "' to payload '" + LENGTH_1_STRING_ALPHA + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_1_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_1_STRING_ALPHA);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_8_STRING_ALPHA + "' to payload '" + LENGTH_8_STRING_ALPHA + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_8_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_8_STRING_ALPHA);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_10_STRING_ALPHA + "' to payload '" + LENGTH_10_STRING_ALPHA + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_10_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_10_STRING_ALPHA);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_1_STRING_NUM_0 + "' to payload '" + LENGTH_1_STRING_NUM_0 + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_1_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_1_STRING_NUM_0);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_8_STRING_NUM_0 + "' to payload '" + LENGTH_8_STRING_NUM_0 + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_8_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_8_STRING_NUM_0);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_10_STRING_NUM_0 + "' to payload '" + LENGTH_10_STRING_NUM_0 + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_10_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_10_STRING_NUM_0);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_1_STRING_NUM_1 + "' to payload '" + LENGTH_1_STRING_NUM_1 + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_1_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_1_STRING_NUM_1);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_8_STRING_NUM_1 + "' to payload '" + LENGTH_8_STRING_NUM_1 + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_8_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_8_STRING_NUM_1);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_1_LENGTH_10_STRING_NUM_1 + "' to payload '" + LENGTH_10_STRING_NUM_1 + "', width 1", function () {
+        var payload = rpcUtils.unLPack(LPACK_1_LENGTH_10_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_10_STRING_NUM_1);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_1_STRING_ALPHA + "' to payload '" + LENGTH_1_STRING_ALPHA + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_1_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_1_STRING_ALPHA);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_8_STRING_ALPHA + "' to payload '" + LENGTH_8_STRING_ALPHA + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_8_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_8_STRING_ALPHA);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_10_STRING_ALPHA + "' to payload '" + LENGTH_10_STRING_ALPHA + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_10_STRING_ALPHA);
+        expect(payload).toBe(LENGTH_10_STRING_ALPHA);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_1_STRING_NUM_0 + "' to payload '" + LENGTH_1_STRING_NUM_0 + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_1_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_1_STRING_NUM_0);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_8_STRING_NUM_0 + "' to payload '" + LENGTH_8_STRING_NUM_0 + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_8_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_8_STRING_NUM_0);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_10_STRING_NUM_0 + "' to payload '" + LENGTH_10_STRING_NUM_0 + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_10_STRING_NUM_0);
+        expect(payload).toBe(LENGTH_10_STRING_NUM_0);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_1_STRING_NUM_1 + "' to payload '" + LENGTH_1_STRING_NUM_1 + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_1_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_1_STRING_NUM_1);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_8_STRING_NUM_1 + "' to payload '" + LENGTH_8_STRING_NUM_1 + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_8_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_8_STRING_NUM_1);
+    });
+
+    it ("should correctly un-LPack '" + LPACK_3_LENGTH_10_STRING_NUM_1 + "' to payload '" + LENGTH_10_STRING_NUM_1 + "', width 3", function () {
+        var payload = rpcUtils.unLPack(LPACK_3_LENGTH_10_STRING_NUM_1);
+        expect(payload).toBe(LENGTH_10_STRING_NUM_1);
+    });
+
+});
+
+
+var LENGTH_1_STRING_ALPHA = 'A';
+var LENGTH_8_STRING_ALPHA = 'AbCdEfGh';
+var LENGTH_10_STRING_ALPHA = 'AbCdEfGhIj';
+var LENGTH_1_STRING_NUM_0 = '0';
+var LENGTH_1_STRING_NUM_1 = '1';
+var LENGTH_8_STRING_NUM_0 = '01234567';
+var LENGTH_8_STRING_NUM_1 = '12345678';
+var LENGTH_10_STRING_NUM_0 = '0123456789';
+var LENGTH_10_STRING_NUM_1 = '1234567890';
+
+var SPACK_LENGTH_1_STRING_ALPHA = VistaJSLibrary.prependCount(LENGTH_1_STRING_ALPHA);
+var SPACK_LENGTH_8_STRING_ALPHA = VistaJSLibrary.prependCount(LENGTH_8_STRING_ALPHA);
+var SPACK_LENGTH_10_STRING_ALPHA = VistaJSLibrary.prependCount(LENGTH_10_STRING_ALPHA);
+var SPACK_LENGTH_1_STRING_NUM_0 = VistaJSLibrary.prependCount(LENGTH_1_STRING_NUM_0);
+var SPACK_LENGTH_1_STRING_NUM_1 = VistaJSLibrary.prependCount(LENGTH_1_STRING_NUM_1);
+var SPACK_LENGTH_8_STRING_NUM_0 = VistaJSLibrary.prependCount(LENGTH_8_STRING_NUM_0);
+var SPACK_LENGTH_8_STRING_NUM_1 = VistaJSLibrary.prependCount(LENGTH_8_STRING_NUM_1);
+var SPACK_LENGTH_10_STRING_NUM_0 = VistaJSLibrary.prependCount(LENGTH_10_STRING_NUM_0);
+var SPACK_LENGTH_10_STRING_NUM_1 = VistaJSLibrary.prependCount(LENGTH_10_STRING_NUM_1);
+
+var LPACK_1_LENGTH_1_STRING_ALPHA = VistaJSLibrary.strPack(LENGTH_1_STRING_ALPHA, 1);
+var LPACK_1_LENGTH_8_STRING_ALPHA = VistaJSLibrary.strPack(LENGTH_8_STRING_ALPHA, 1);
+var LPACK_1_LENGTH_10_STRING_ALPHA = VistaJSLibrary.strPack(LENGTH_10_STRING_ALPHA, 1);
+var LPACK_1_LENGTH_1_STRING_NUM_0 = VistaJSLibrary.strPack(LENGTH_1_STRING_NUM_0, 1);
+var LPACK_1_LENGTH_1_STRING_NUM_1 = VistaJSLibrary.strPack(LENGTH_1_STRING_NUM_1, 1);
+var LPACK_1_LENGTH_8_STRING_NUM_0 = VistaJSLibrary.strPack(LENGTH_8_STRING_NUM_0, 1);
+var LPACK_1_LENGTH_8_STRING_NUM_1 = VistaJSLibrary.strPack(LENGTH_8_STRING_NUM_1, 1);
+var LPACK_1_LENGTH_10_STRING_NUM_0 = VistaJSLibrary.strPack(LENGTH_10_STRING_NUM_0, 1);
+var LPACK_1_LENGTH_10_STRING_NUM_1 = VistaJSLibrary.strPack(LENGTH_10_STRING_NUM_1, 1);
+
+var LPACK_3_LENGTH_1_STRING_ALPHA = VistaJSLibrary.strPack(LENGTH_1_STRING_ALPHA, 3);
+var LPACK_3_LENGTH_8_STRING_ALPHA = VistaJSLibrary.strPack(LENGTH_8_STRING_ALPHA, 3);
+var LPACK_3_LENGTH_10_STRING_ALPHA = VistaJSLibrary.strPack(LENGTH_10_STRING_ALPHA, 3);
+var LPACK_3_LENGTH_1_STRING_NUM_0 = VistaJSLibrary.strPack(LENGTH_1_STRING_NUM_0, 3);
+var LPACK_3_LENGTH_1_STRING_NUM_1 = VistaJSLibrary.strPack(LENGTH_1_STRING_NUM_1, 3);
+var LPACK_3_LENGTH_8_STRING_NUM_0 = VistaJSLibrary.strPack(LENGTH_8_STRING_NUM_0, 3);
+var LPACK_3_LENGTH_8_STRING_NUM_1 = VistaJSLibrary.strPack(LENGTH_8_STRING_NUM_1, 3);
+var LPACK_3_LENGTH_10_STRING_NUM_0 = VistaJSLibrary.strPack(LENGTH_10_STRING_NUM_0, 3);
+var LPACK_3_LENGTH_10_STRING_NUM_1 = VistaJSLibrary.strPack(LENGTH_10_STRING_NUM_1, 3);
