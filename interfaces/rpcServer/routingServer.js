a4da28d9252814f7b35de75e97953e31f5c50308
Sean Ishii <sishii@hawaiirg.com>
2016-07-27 09:40:46 -1000

    Issue_54 BROKEN: Can pass the TCPConnect and accept but server stops receiving data after first RPC. This code does not work. It is only being added to keep history.

--- interfaces/rpcServer/routingServer.js
+++ interfaces/rpcServer/routingServer.js
@@ -39,10 +39,14 @@ function handleConnection(conn) {
     conn.on('error', onConnectedError);
 
     var brokerSocket = new net.Socket();
+    brokerSocket.isConnected = false;
+    var self = this;
     brokerSocket.on('error', onBrokerConnectionError);
     brokerSocket.on('close', onBrokerConnectionClose);
     brokerSocket.setEncoding('utf8');
-    brokerSocket.connect(configuration.port, configuration.host);
+    brokerSocket.connect(configuration.port, configuration.host, function() {
+        brokerSocket.isConnected = true;
+    });
 
     function onConnectedData(data) {
         chunk += data;
@@ -56,7 +60,7 @@ function handleConnection(conn) {
         // loop for each packet in the data chunk
         while (eotIndex > -1) {
             // get a packet from the chunk (without the EOT)
-            var rpcPacket = chunk.substr(0, eotIndex);
+            var rpcPacket = chunk.substr(0, eotIndex + 1);
             // remove the RPC packet from the chunk
             chunk = chunk.substring(eotIndex + 1)
             // find the end of the next RPC packet
@@ -73,33 +77,11 @@ function handleConnection(conn) {
             // check if RPC is supported to pass to MVDM
 
             // pass the unsupported RPC to the legacy broker
-            async.series([
-                function(callback) {
-                    brokerSocket.on('data', onBrokerConnectionData); // RpcTask receive
-                    brokerSocket.write(rpcPacket);
-                    callback();
-                }
-            ], function (err) {
-                if (err) {
-                    LOGGER.error("Error in writing to broker %s", err);
-                }
-            });
-
-
-            //var rpcCommand = {
-            //    rpc: rpcPacket,
-            //    process: function(data) {
-            //        return data;
-            //    }
-            //};
-            //var rpcTask = new VistaJSLibrary.RpcTask(LOGGER, rpcCommand, brokerSocket);
-            //var execList = [];
-            //execList.push(rpcTask.execute.bind(rpcCommand));
-            //async.series(execList, function(error, result) {
-            //    if (!error) {
-            //        conn.write(result);
-            //    }
-            //});
+            if (brokerSocket.isConnected) {
+                brokerSocket.on('data', onBrokerConnectionData);
+                LOGGER.info("Writing from sniffer to broker message: %s, length %s", rpcPacket, rpcPacket.length);
+                brokerSocket.write(rpcPacket);
+            }
 
         }
 
@@ -111,7 +93,6 @@ function handleConnection(conn) {
     }
 
     function onConnectedError(err) {
-        LOGGER.error('Connection %s error: %s', remoteAddress, err.message);
         LOGGER.error('Connection %s error: %s', remoteAddress, err.message);
     }
 
@@ -158,12 +139,11 @@ function handleConnection(conn) {
     }
 
     function onBrokerConnectionClose() {
-        LOGGER.info('connection from %s closed', remoteAddress);
+        LOGGER.info('BrokerConnection closed');
     }
 
     function onBrokerConnectionError(err) {
-        LOGGER.error('Connection %s error: %s', remoteAddress, err.message);
-        LOGGER.error('Connection %s error: %s', remoteAddress, err.message);
+        LOGGER.error('BrokerConnection error: %s', err.message);
     }
 
 
@@ -186,6 +166,161 @@ function handleConnection(conn) {
     }
 
 }
+
+
+
+
+
+/***************** STUFF COPIED FROM VISTAJSLIBRARY *************************/
+function RpcClient(logger, configuration, rpcCommandList, callback) {
+    if (!(this instanceof RpcClient)) {
+        return new RpcClient(logger, configuration, rpcCommandList, callback);
+    }
+
+    this.logger = logger || require('bunyan').createLogger({
+            name: 'RpcClient',
+            level: 'info'
+        });
+    this.configuration = configuration;
+    this.rpcTaskList = [];
+    this.execList = [];
+    this.callback = callback;
+    this.socket = new net.Socket();
+
+    if (!rpcCommandList || !_.isArray(rpcCommandList)) {
+        throw new Error('RpcCommandList must be an array of RpcCommand objects');
+    }
+
+    var self = this;
+    rpcCommandList.forEach(function(rpcCommand) {
+        var rpcTask = new RpcTask(self.logger, rpcCommand, self.socket);
+        self.rpcTaskList.push(rpcTask);
+        self.execList.push(rpcTask.execute.bind(rpcCommand));
+    });
+
+}
+
+RpcClient.prototype = {
+    constructor: RpcClient,
+
+    complete: function(error, results) {
+        this.logger.debug('RpcClient.complete()');
+        this.close();
+        this.callback(error, results);
+        // if !error, get last - 1 result and callback
+    },
+
+    onError: function(error) {
+        this.logger.debug('RpcClient.onError()');
+        this.close();
+        this.callback(error, null);
+    },
+
+    onClose: function() {
+        this.logger.debug('RpcClient.onClose()');
+        this.close();
+    },
+
+    close: function() {
+        this.logger.debug('RpcClient.close()');
+        this.socket.removeAllListeners();
+        this.socket.end();
+        this.socket.destroy();
+    },
+
+    start: function() {
+        this.logger.debug('RpcClient.start()');
+        var self = this;
+
+        this.socket.on('error', this.onError.bind(this));
+        this.socket.on('close', this.onClose.bind(this));
+
+        this.socket.setEncoding('utf8');
+        this.socket.connect(this.configuration.port, this.configuration.host, function() {
+            async.series(self.execList, self.complete.bind(self));
+        });
+    },
+};
+
+
+function RpcTask(logger, rpcCommand, socket) {
+    if (!(this instanceof RpcTask)) {
+        return new RpcTask(logger, rpcCommand, socket);
+    }
+
+    this.logger = logger || require('bunyan').createLogger({
+            name: 'RpcClient',
+            level: 'info'
+        });
+    this.buffer = '';
+    this.socket = socket;
+    this.rpcCommand = rpcCommand;
+
+    this.execute = this.execute.bind(this);
+    this.receive = this.receive.bind(this);
+}
+
+RpcTask.prototype = {
+    constructor: RpcTask,
+
+    execute: function(callback) {
+        this.logger.debug('RpcClient.execute()');
+        this.callback = callback;
+        this.socket.on('data', this.receive.bind(this));
+        this.logger.debug(this.rpcCommand.rpc.replace(/[^ -~]+/, '')); // /[\cA-\cZ]+/  /[\x00-\x1F]+/
+        this.socket.write(this.rpcCommand.rpc);
+    },
+
+    receive: function(data) {
+        this.logger.debug('RpcClient.receive()');
+        var result;
+        var error;
+
+        this.buffer += data;
+
+        if (this.buffer.indexOf(EOT) !== -1) {
+            if (this.buffer[0] !== NUL) {
+                this.logger.trace(data);
+                error = new Error('VistA SECURITY error: ' + extractSecurityErrorMessage(this.buffer));
+            } else if (this.buffer[1] !== NUL) {
+                this.logger.trace(data);
+                error = new Error('VistA APPLICATION error: ' + this.buffer);
+            }
+
+            this.buffer = this.buffer.substring(2);
+
+            if (this.buffer.indexOf('M  ERROR') !== -1) {
+                this.logger.trace(this.buffer);
+                error = new Error(this.buffer);
+            }
+
+            result = this.buffer.substring(0, this.buffer.indexOf(EOT));
+            this.socket.removeAllListeners('data');
+            this.buffer = '';
+            if (!error) {
+                try {
+                    result = this.rpcCommand.process(result);
+                } catch (err) {
+                    error = err;
+                }
+            }
+
+            this.logger.trace(error);
+            this.logger.trace('RpcClient result: ' + util.inspect(result, {
+                    depth: null
+                }));
+
+            this.callback(error, result);
+        }
+    }
+};
+
+
+
+
+
+
+
 
 
 
