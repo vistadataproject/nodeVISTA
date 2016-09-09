3e71f89d83671abe1cb0e5c68004477144ee8640
Sean Ishii <sishii@hawaiirg.com>
2016-09-09 08:29:27 -1000

    Issue_54 BROKEN reverting the refactor from c85fb59 which was committed for reference

--- interfaces/rpcLockerJobServer/rpcLWorker.js
+++ interfaces/rpcLockerJobServer/rpcLWorker.js
@@ -2,33 +2,89 @@ var _ = require('underscore');
 var util = require('util');
 var nodem = require('nodem');
 var VDM = require('../../../VDM/prototypes/vdm');
-
+var mvdmModelAllergy = require('../../../VDM/prototypes/allergies/mvdmAllergiesModel').mvdmModel;
 var MVDM = require('../../../VDM/prototypes/mvdm');
 var testAllergies = require('../../../VDM/prototypes/allergies/vdmTestAllergies'); // want to use test allergies
-
+var allergyUtils = require("../../../VDM/prototypes/allergies/allergyUtils");
 var localRPCRunner = require('../../../VDM/prototypes/localRPCRunner');
 var fmql = require('../../../VDM/prototypes/fmql');
 var vprE = require('../../../VDM/prototypes/vprEmulate/vprE');
 var vpr = require('../../../VDM/prototypes/vpr');
+var vprAllergyEmulator = require('../../../VDM/prototypes/vprEmulate/vprAllergyEmulator');
+var mvdmModelProblem = require('../../../VDM/prototypes/problems/mvdmProblemsModel').mvdmModel;
+var vprProblemEmulator = require('../../../VDM/prototypes/vprEmulate/vprProblemEmulator');
+var vitalUtils = require('../../../VDM/prototypes/vitals/vitalUtils');
+var mvdmModelVitals = require('../../../VDM/prototypes/vitals/mvdmVitalsModel').mvdmModel;
+var vprVitalsEmulator = require('../../../VDM/prototypes/vprEmulate/vprVitalsEmulator');
 
 var fs = require('fs');
 var os = require("os");
 var fileman = require('../../../VDM/prototypes/fileman');
-
+var vdmUtils = require("../../../VDM/prototypes/vdmUtils");
 process.env.gtmroutines = process.env.gtmroutines + '../../../VDM/prototypes'; // make VDP MUMPS available
 var db; // for afterAll
 db = new nodem.Gtm();
 db.open();
 var vprDataVersion; // for XML vs JSON
+var allergyModel = require('../../../VDM/prototypes/allergies/vdmAllergiesModel').vdmModel;
+var documentModel = require('../../../VDM/prototypes/documents/vdmDocumentsModel').vdmModel;
+var visitModel = require('../../../VDM/prototypes/visits/vdmVisitsModel').vdmModel;
+var vdmModelAllergy = allergyModel.concat(documentModel, visitModel);
+var vdmModelProblem = require('../../../VDM/prototypes/problems/vdmProblemsModel').vdmModel;
+var vdmModelVitals = require('../../../VDM/prototypes/vitals/vdmVitalsModel').vdmModel;
+var rpcLProblemModel = require('../../../VDM/prototypes/problems/rpcLProblemModel').rpcLModel;
+var DUZ = 55; // Should match Robert Alexander used in JSON tests but may not.
+var facilityCode = 2957;
 
-var CONFIG  = require('./cfg/config.js');
-var DUZ = CONFIG.USER.DUZ; // Should match Robert Alexander used in JSON tests but may not.
-var facilityCode = CONFIG.FACILITY.ID;
-
-var LockerModelUtils = require('./lockerModelUtils')
-var lockerModelUtils = new LockerModelUtils(CONFIG);
 
 var rpcL = require('../../../VDM/prototypes/rpcL');
+var rpcLAllergyMappings = require('../../../VDM/prototypes/allergies/rpcAllergiesLocker');
+var rpcVitalEmulate = require('../../../VDM/prototypes/vitals/rpcVitalLocker');
+
+function setModels(domain) {
+    if (domain === 'allergy') {
+        VDM.setDBAndModel(db, vdmModelAllergy);
+        VDM.setUserAndFacility("200-55", "4-2957"); // note that 4-2957 would come from 200-55 if left out
+        MVDM.setModel(mvdmModelAllergy);
+        vprE.setVprMappings(vprAllergyEmulator, '1.05');
+        // Note: allergy doesn't note the facility, just the user logged in but can get it (need for full creation events)
+        // VDM.setUserAndFacility("200-" + DUZ, "4-" + facilityCode);
+        rpcL.setRpcMappings(rpcLAllergyMappings);
+
+    } else if (domain === 'problem') {
+        VDM.setDBAndModel(db, vdmModelProblem);
+        VDM.setUserAndFacility("200-55", "4-2957"); // note that 4-2957 would come from 200-55 if left out
+
+        MVDM.setModel(mvdmModelProblem);
+        vprE.setVprMappings(vprProblemEmulator, '1.05');
+
+        DUZ = 55; // Matches Robert Alexander
+
+        patientIen = 25;
+
+        var fmqlRes = fmql.query(db, "DESCRIBE 2-" + patientIen);
+
+        var patientName = fmqlRes.results[0].name.value;
+        var last4 = fmqlRes.results[0].social_security_number.value.substring(5);
+
+        patientArgs = patientIen + '^' + patientName + '^' + last4 + '^';
+        facilityCode = '2957'; //OSEHRA VistA facility code
+
+        rpcL.setDBAndModels(db, {
+            rpcLModel: rpcLProblemModel,
+            vdmModel: vdmModelProblem,
+            mvdmModel: mvdmModelProblem
+        });
+        rpcL.setUserAndFacility("200-" + DUZ, "4-" + facilityCode); // note that 4-2957 would come from 200-55 if left out
+
+    } else if (domain === 'vitals') {
+        VDM.setDBAndModel(db, vdmModelVitals);
+        VDM.setUserAndFacility("200-55", "4-2957"); // note that 4-2957 would come from 200-55 if left out
+        MVDM.setModel(mvdmModelVitals);
+        vprE.setVprMappings(vprVitalsEmulator, '1.05');
+        rpcL.setRpcMappings(rpcVitalEmulate.rpcMappings);
+    }
+}
 
 //for emulator only
 //we need to know the domain in order to populate the domain specific mappings
@@ -42,7 +98,7 @@ function getDomain(rpcName) {
 
 function callVpr(messageObj) {
     var domain = messageObj.query.domain;
-    lockerModelUtils.setModels(domain);
+    setModels(domain);
     var rpcArgs = messageObj.query.rpcArgs.split(',');
     if (!validateArgs(rpcArgs)) {
         return 'Error: invalid args';
@@ -82,7 +138,7 @@ function callRpc(messageObj) {
         return 'Error: invalid args';
     }
     var domain = getDomain(messageObj.query.rpc);
-    lockerModelUtils.setModels(domain);
+    setModels(domain);
     var rpc = messageObj.query.rpc; //'ORQQAL DETAIL', ORQQPL DETAIL
     var rpcsLocked = messageObj.query.rpcsLocked;
     if (rpcsLocked === 'off' || !rpcL.isRPCSupported(rpc)) {
