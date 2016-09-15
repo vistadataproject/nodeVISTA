150b78bdbe4946c6829d2fc4d317f59cdd81da8f
Sean Ishii <sishii@hawaiirg.com>
2016-09-15 11:33:25 -1000

    Issue_54 added to reject non-conforming TCPConnect

--- interfaces/rpcServer/unsupportedRPCs.js
+++ interfaces/rpcServer/unsupportedRPCs.js
@@ -4,7 +4,7 @@ var CONFIG = require('./cfg/config.js');
 var unsupportedRpcs = new HashMap();
 
 unsupportedRpcs.set("TCPConnect", "\u0000\u0000accept\u0004");
-
+unsupportedRpcs.set("#REJECT#", "\u0000\u0000reject\u0004");
 unsupportedRpcs.set("#BYE#", "\u0000\u0000#BYE#\u0004");
 
 module.exports = unsupportedRpcs;
