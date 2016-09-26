1ddd0daabf6a9c721c2aede4cc1580d336869669
Sean Ishii <sishii@hawaiirg.com>
2016-09-26 11:47:43 -1000

    Issue_54 moved mvdm event proxying/emitting to QWorker/rpcServer

--- interfaces/rpcServer/eventManager.js
+++ interfaces/rpcServer/eventManager.js
@@ -4,7 +4,6 @@
 var _ = require('underscore');
 var util = require('util');
 var EventEmitter = require('events');
-var MVDM = require('../../../VDM/prototypes/mvdm');
 
 function EventHandler() {
    EventEmitter.call(this);
@@ -14,35 +13,5 @@ function EventHandler() {
 util.inherits(EventHandler, EventEmitter);
 
 var eventHandler = new EventHandler();
-
-//proxy MVDM events
-
-MVDM.on('create', function(mvdmData) {
-   eventHandler.emit('mvdmCreate', mvdmData);
-});
-
-MVDM.on('describe', function(mvdmData) {
-   eventHandler.emit('mvdmDescribe', mvdmData);
-});
-
-MVDM.on('list', function(mvdmData) {
-   eventHandler.emit('mvdmList', mvdmData);
-});
-
-MVDM.on('update', function(mvdmData) {
-   eventHandler.emit('mvdmUpdate', mvdmData);
-});
-
-MVDM.on('remove',function(mvdmData) {
-   eventHandler.emit('mvdmRemove', mvdmData);
-});
-
-MVDM.on('unremoved', function(mvdmData) {
-   eventHandler.emit('mvdmUnremoved', mvdmData);
-});
-
-MVDM.on('delete', function(mvdmData) {
-   eventHandler.emit('mvdmDelete', mvdmData);
-});
 
 module.exports = eventHandler;
