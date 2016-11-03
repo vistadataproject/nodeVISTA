36b387069f01f9dc931097e46d8ed972d06b8d3a
Steve Bruno <sbruno@hawaiirg.com>
2016-11-03 10:27:11 -0500

    cleanups, top 20 no longer using backgrid

--- interfaces/rpcServer/static/js/stats/rpcStatCollection.js
+++ interfaces/rpcServer/static/js/stats/rpcStatCollection.js
@@ -3,28 +3,38 @@ define([
    'jquery',
    'underscore',
    'backbone',
-   'stats/rpcStatModel',
-   'backbone.paginator'
+   'stats/rpcStatModel'
 ], function ($, _, Backbone, RPCStatModel) {
    'use strict';
 
-   var RPCStatCollection = Backbone.PageableCollection.extend({
+   var RPCStatCollection = Backbone.Collection.extend({
       model: RPCStatModel,
       mode: 'client',
-      // Initial pagination states
-      state: {
-         pageSize: 20,
-         firstPage: 0,
-         currentPage: 0,
-         sortKey: 'count',
-         order: 1
+      comparator: function(a, b) {
+         var aCount = a.get('count');
+         var bCount = b.get('count');
+         var aName = a.get('name');
+         var bName = b.get('name');
+
+         if (aCount > bCount) {
+            return 1;
+         } else if (aCount === bCount) {
+            //if counts are equal, compare by name
+            if (aName < bName) {
+               return 1;
+            } if (aName === bName) {
+               return 0;
+            }
+         }
+
+         return -1;
       },
 
       consumeEvent: function(eventModel) {
          var rpcName = eventModel.get('rpcName');
          var runner = eventModel.get('runner');
 
-         var rpc = this.fullCollection.find(function(model) {return model.get('name') === rpcName});
+         var rpc = this.find(function(model) {return model.get('name') === rpcName});
 
          if (!rpc) {
             this.add(new RPCStatModel({
@@ -39,7 +49,7 @@ define([
 
       total: function() {
          var total = 0;
-         this.fullCollection.forEach(function(rpc) {
+         this.forEach(function(rpc) {
             total += rpc.get('count');
          });
 
@@ -47,13 +57,13 @@ define([
       },
 
       distinctTotal: function() {
-         return this.fullCollection.size();
+         return this.size();
       },
 
       distinctLockedTotal: function() {
          var distinctLocked = 0;
 
-         this.fullCollection.forEach(function(rpc) {
+         this.forEach(function(rpc) {
             if (rpc.get('runner') === 'mvdmLocked') {
                distinctLocked++;
             }
@@ -65,13 +75,21 @@ define([
       lockedTotal: function() {
          var locked = 0;
 
-         this.fullCollection.forEach(function(rpc) {
+         this.forEach(function(rpc) {
             if (rpc.get('runner') === 'mvdmLocked') {
                locked += rpc.get('count');
             }
          });
 
          return locked;
+      },
+
+      top: function(num) {
+         if (num < 1) {
+            throw new Error('Number parameter must be greater than zero.')
+         }
+
+         return this.toJSON().reverse().splice(0, num);
       }
    });
 
--- interfaces/rpcServer/static/js/stats/statsView.js
+++ interfaces/rpcServer/static/js/stats/statsView.js
@@ -24,93 +24,33 @@ define([
    'backgrid',
    'stats/rpcStatModel',
    'stats/rpcStatCollection',
-   'text!stats/coreStats.hbs',
    'text!stats/stats.hbs',
    'eventBus',
    'templateHelpers',
    'backgridCustomCells'
-], function ($, _, Backbone, Handlebars, Backgrid, RPCStatModel, RPCStatCollection, coreStatsTemplate, statsTemplate, EventBus) {
+], function ($, _, Backbone, Handlebars, Backgrid, RPCStatModel, RPCStatCollection, statsTemplate, EventBus) {
    'use strict';
    var StatsView = Backbone.View.extend({
 
       template: Handlebars.compile(statsTemplate),
 
       initialize: function () {
-         this.coreStatsTemplate = Handlebars.compile(coreStatsTemplate);
-
-         this.grid = new Backgrid.Grid({
-            columns: [{
-               name: 'name',
-               label: 'RPC',
-               sortable: false,
-               editable: false,
-               cell:'string'
-            }, {
-               name: 'runner',
-               label: 'Path',
-               editable: false,
-               sortable: false,
-               cell: Backgrid.HtmlCell,
-               formatter: _.extend({}, Backgrid.CellFormatter.prototype, {
-                  fromRaw: function (rawValue, model) {
-                     var retVal = rawValue;
-
-                     if (rawValue === 'rpcRunner') {
-                        retVal = 'RPC Runner';
-                     } else if (rawValue === 'mvdmLocked') {
-                        retVal = 'MVDM Locked';
-                     } else if (rawValue === 'server') {
-                        retVal = 'Server';
-                     }
-
-                     return Backgrid.HtmlCell.formatAsHtml(retVal, model);
-                  }
-               })
-            }, {
-               name: 'count',
-               label: '# of Calls',
-               sortable: false,
-               editable: false,
-               cell: 'integer'
-            }],
-            collection: RPCStatCollection
-         });
-
-         this.paginator = new Backgrid.Extension.Paginator({
-            collection: RPCStatCollection,
-            goBackFirstOnSort: false
-         });
 
          this.listenTo(EventBus, 'statsEvent', function(statsModel) {
-            this.renderCoreStats();
-            this.grid.sort("count", "descending");
+            this.render();
          });
       },
 
       render: function() {
-         this.$el.html(this.template());
-
-         this.renderCoreStats();
-
-         //render table
-         this.$el.find('#rpc-table').append(this.grid.render().el);
-
-         //apply bootstrap table styles to grid
-         this.$el.find('.backgrid').addClass('table table-condensed table-striped table-bordered');
-
-         this.grid.sort("count", "descending");
-
-         return this;
-      },
-
-      renderCoreStats: function() {
-
-         this.$el.find('#core-stats').html(this.coreStatsTemplate({
+         this.$el.html(this.template({
             total: RPCStatCollection.total(),
             distinct: RPCStatCollection.distinctTotal(),
             distinctLocked: RPCStatCollection.distinctLockedTotal(),
-            locked: RPCStatCollection.lockedTotal()
+            locked: RPCStatCollection.lockedTotal(),
+            topList: RPCStatCollection.top(20)
          }));
+
+         return this;
       },
 
       onClose: function () {
