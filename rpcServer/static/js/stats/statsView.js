882610cf8a8bad9e737aa2eff026c9d040fb0c6f
Steve Bruno <sbruno@hawaiirg.com>
2016-12-05 15:24:21 -0600

    breaking up stats display

--- rpcServer/static/js/stats/statsView.js
+++ rpcServer/static/js/stats/statsView.js
@@ -25,19 +25,25 @@ define([
    'stats/rpcStatModel',
    'stats/rpcStatCollection',
    'text!stats/stats.hbs',
+   'text!stats/keyStats.hbs',
+   'text!stats/top20.hbs',
    'eventBus',
    'templateHelpers',
    'backgridCustomCells'
-], function ($, _, Backbone, Handlebars, Backgrid, RPCStatModel, RPCStatCollection, statsTemplate, EventBus) {
+], function ($, _, Backbone, Handlebars, Backgrid, RPCStatModel, RPCStatCollection, statsTemplate, keyStatsTemplate, top20Template, EventBus) {
    'use strict';
    var StatsView = Backbone.View.extend({
 
       template: Handlebars.compile(statsTemplate),
+      keyStatsTemplate: Handlebars.compile(keyStatsTemplate),
+      top20Template: Handlebars.compile(top20Template),
 
       initialize: function () {
 
          this.listenTo(EventBus, 'statsEvent', function(statsModel) {
-            this.render();
+            this.renderKeyStats();
+            this.renderTop20();
+
          });
       },
 
@@ -50,9 +56,25 @@ define([
             topList: RPCStatCollection.top(20)
          }));
 
+         this.renderKeyStats();
+         this.renderTop20();
+
          return this;
       },
 
+      renderKeyStats: function() {
+         this.$el.find('.keyStats').html(this.keyStatsTemplate({
+            total: RPCStatCollection.total(),
+            distinct: RPCStatCollection.distinctTotal(),
+            distinctLocked: RPCStatCollection.distinctLockedTotal(),
+            locked: RPCStatCollection.lockedTotal()
+         }));
+      },
+      renderTop20: function() {
+         this.$el.find('.top20').html(this.top20Template({
+            topList: RPCStatCollection.top(20)
+         }));
+      },
       onClose: function () {
 
       }
