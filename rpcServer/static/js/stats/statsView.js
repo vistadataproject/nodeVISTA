c13f9facf069978c8a900667c96437f234a3c663
Steve Bruno <sbruno@hawaiirg.com>
2016-12-07 18:44:36 -0600

    removing TODO

--- rpcServer/static/js/stats/statsView.js
+++ rpcServer/static/js/stats/statsView.js
@@ -1,20 +1,3 @@
-/**
- * Done:
- *
- * Total number of RPCs invoked
- * Number of calls that are locked
- * Number of distinct RPCs invoked
- * Number of distinct RPCs that are locked
- * Top Ten RPCs invoked (since client start up)
- * Number of calls for each of the distinct RPCs
- *
- * TODO:
- *
- * Categorize RPCs by function (utility RPCs, write RPCs, read RPCs, Authorization RPCs)
- * Break down the utility RPCs - ones that we'll never lock and those that we won't be backing with the model
- */
-
-
 /*global define*/
 define([
    'jquery',
