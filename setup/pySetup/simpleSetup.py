0763b06dcff49fe6de2ce051bcc0df32bed89410
Conor Dowling <conor-dowling@caregraf.com>
2017-01-27 04:28:26 +0000

    setup of vitals etc

--- setup/pySetup/simpleSetup.py
+++ setup/pySetup/simpleSetup.py
@@ -69,9 +69,10 @@ def simpleSetup():
 
     try:
         print "Finally complete PARAMETER setup (VITALs/CAPRI) ..."
+        time.sleep(10)
+        VistA=ConnectToMUMPS(LOGFILE) # reset up VISTA
         completeVitalsSetup(VistA)
     except Exception as e:
-        print e
         print "EXIT_PYS_CANT_COMPLETE_PARAMETER_SETUP"
         return
 
@@ -223,26 +224,20 @@ def completeVitalsSetup(VistA):
     # 1. GMV MANAGER RPC
     VistA.wait(PROMPT,60)
     VistA.write('S TDVITALS=\"TPR, BP|1:0;5:0;3:0;2:0;21:0;8:0;22:0;9:0\"')
-    VistA.write('D ADD^XPAR(\"SYS\","\GMV TEMPLATE\",\"DAILY VITALS\",TDVITALS)')
+    VistA.write('D ADD^XPAR("SYS","GMV TEMPLATE","DAILY VITALS",TDVITALS)')
+
     # 2. GMV USER RPC - must be set per user so done later
     VistA.wait(PROMPT,60)
     VistA.IEN('NEW PERSON','ALEXANDER,ROBERT')
     VistA.write('S DUZ=' + VistA.IENumber);
     VistA.write('S UTVITAL=\"00;DIC(4.2,|DAILY VITALS\"')
-    VistA.write('D ADD^XPAR(\"USR\","\GMV USER DEFAULTS\",\"DefaultTemplate\",UTVITAL)')
+    VistA.write('D ADD^XPAR("USR","GMV USER DEFAULTS","DefaultTemplate",UTVITAL)')
 
-   
-    # GMV USER RPC - must be set per user so done here and not in vital setup above
-    VistA.wait(PROMPT,60)
-    VistA.IEN('NEW PERSON','ALEXANDER,ROBERT')
-    VistA.write('S DUZ=' + VistA.IENumber);
-    VistA.write('S UTVITAL=\"00;DIC(4.2,|DAILY VITALS\"')
-    VistA.write('D ADD^XPAR(\"USR\","\GMV USER DEFAULTS\",\"DefaultTemplate\",UTVITAL)')
-   
-    # Fix OSEHRA Capri - VA wants N to leave Old Style Capri enabled. OSEHRA's
+    # 3. Fix OSEHRA Capri - VA wants N to leave Old Style Capri enabled. OSEHRA's
     # partial domain resetting from FOIA to OSEHRA leaves this parameter unset for the new
-    # domain
-    VistA.write('D ADD^XPAR(\"SYS\",\"XU522\",1,\"N\")')
+    # domain. Our nodeVISTA setup would suffer the same way
+    VistA.wait(PROMPT,60)
+    VistA.write('D ADD^XPAR("SYS","XU522",1,"N")')
 
 def main():
     simpleSetup()
