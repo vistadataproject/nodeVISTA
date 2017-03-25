a6e982914a880615f6af24842fd05b891798e054
Edison Ting <eting@hawaiirg.com>
2017-03-24 16:22:50 -1000

    Adding util to get all attributes name from JSON file

---
+++ setup/jsSetup/getJsonAttributes.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const _ = require('lodash');
+const fs = require('fs');
+
+// read JSON object from data.json file
+const usersRecord = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+
+// This util is a quick way to get all the attribute name for a given JSON object
+const nextLevel = function nextLevel(obj, results, level) {
+    let depth = level;
+    if (_.isEmpty(obj)) return;
+    if (_.isObject(obj)) {
+        _.each(Object.keys(obj), (field) => {
+            if (_.isNaN(parseInt(field, 10))) {
+                results.push(`(${level}) ${field}`);
+            }
+        });
+        depth += (Object.keys(obj).length > 0) ? 1 : 0;
+        _.each(obj, (element) => {
+            nextLevel(element, results, depth);
+        });
+    }
+};
+
+const objectAttributes = [];
+_.each(usersRecord, (element) => {
+    nextLevel(element, objectAttributes, 0);
+});
+
+console.log(_.uniq(objectAttributes, 'id').sort());
