78744ed0ce9fcd729650584ec23e7ed584db3947
Edison Ting <eting@hawaiirg.com>
2017-04-05 20:05:21 -1000

    adding js tool to add new users from a given json

--- setup/jsSetup/addNewUsers.js
+++ setup/jsSetup/addNewUsers.js
@@ -11,6 +11,8 @@ const fs = require('fs');
 const nodem = require('nodem');
 const util = require('util');
 
+// todo: point this to mvdm
+// todo: move this non-clinical
 // sets the path for all mumps GT.M routines (compiled .m files)
 process.env.gtmroutines = `${process.env.gtmroutines} ${vdmUtils.getVdmPath()}`;
 
@@ -34,7 +36,7 @@ const printResult = function printResult(obj) {
 
 // Check if the the user has minimum required field for fileman to create a new user
 const hasMinumumFields = function hasMinumumFields(user) {
-    const minimumFieldToCreate = ['name', 'type', 'service_section', 'initial'];
+    const minimumFieldToCreate = ['name', 'type', 'initial'];
     return !_.some(minimumFieldToCreate, (field) => {
         if (!_.has(user, field)) {
             console.log(`unable to proceed, user ${user.name} missing required '${field}' field to create!`);
@@ -45,14 +47,11 @@ const hasMinumumFields = function hasMinumumFields(user) {
 
 // Filemans allow to update the following fields with no error
 // We first use first level field below to create a user record and then update the level/depth(2) data via VDM update
-const allowedFields = ['name', 'sex', 'ssn', 'type', 'service_section', 'initial',
-    'terminal_type_last_used', 'signature_block_printed_name',
-    'service_section', 'preferred_editor', 'effective_date',
-];
-
-// The following are level/depth(2) data - usually are arrays
-const acceptedUpdatedFields = ['keys', 'primary_menu_option', 'secondary_menu_options',
-    'cprs_tab', 'person_class', 'division',
+const allowedFields = ['name', 'sex', 'ssn', 'type', 'service_section', 'initial', 'service_section',
+    'terminal_type_last_used', 'signature_block_printed_name', 'electronic_signature_code',
+    'file_manager_access_code', 'service_section', 'preferred_editor', 'effective_date', 'access_code', 'verify_code',
+    'keys', 'primary_menu_option', 'secondary_menu_options', 'cprs_tab', 'person_class', 'division', 'multiple_signon',
+    'restrict_patient_selection',
 ];
 
 VDM.setDBAndModel(db, vdmNonClinicalModel);
@@ -63,40 +62,25 @@ VDM.setUserAndFacility(userId, facilityId); // must setup user (DUZ) when creati
 // Read input JSON object from file (getUsersRecord.js)
 const usersRecord = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
 
+
 if (_.every(usersRecord, hasMinumumFields)) { // getUsersRecord.js should have already checked this
     _.each(usersRecord, (user) => {
         const person = {};
 
+        // allowedFields & acceptedUpdatedFields can be combined into one array
         _.each(allowedFields, (field) => {
             if (user[field]) {
-                person.access_code = `${user.initial}1234`;
-                person.verify_code = `${user.initial}1234!!`;
+                // keep this in case we need to do this for mass import
+                // person.access_code = `${user.initial}1234`;
+                // person.verify_code = `${user.initial}1234!!`;
                 person[field] = user[field];
             } else {
                 console.log(`** Warning: User ${user.name} missing ${field}, update will continue...`);
             }
         });
 
-        let res = VDM.create(person);
-
-        if (res.id) {
-            console.log(`${person.name} (${res.id}) created!!`);
-            _.each(acceptedUpdatedFields, (field) => {
-                person.id = res.id;
-                if (user[field]) {
-                    person[field] = user[field];
-                }
-            });
-
-            try {
-                res = VDM.update(person);
-                // uncomment below if you want to see the response from VDM
-                // printResult(res);
-                console.log(`${person.name} (${person.id}) updated!!`);
-            } catch (e) {
-                console.log(`Error updating ${person.id}`, e);
-            }
-        }
+        const res = VDM.create(person);
+        if (res.id) console.log(`${person.name} (${res.id}) created!!`);
     });
 }
 
