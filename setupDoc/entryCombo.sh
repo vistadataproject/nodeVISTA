23074c624cc4f5e750e492f19018f7da3c7fdbe1
conorcaregraf <conor-dowling@caregraf.com>
2018-03-24 20:43:46 -0700

    trying combo start

--- setupDoc/entryCombo.sh
+++ setupDoc/entryCombo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Invoke with ENTRYPOINT [/home/nodevista/entry.sh] - temporary until move FMQL to own container
 /home/nodevista/bin/start.sh
+su vdp -c "pm2 status"
 su vdp -c "pm2 start /home/vdp/FMQL/webservice/fmqlServer.js && pm2 save >> /home/vdp/logs/fmqlStartup.log"
